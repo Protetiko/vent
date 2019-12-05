377e0e442795b92d87d21b3e49a7b41baccaea65
David Sennerlöv <ndavidn@gmail.com>
2019-12-05 15:50:05 +0100

    Change to version from Core

--- lib/vent/publishers/test_publisher.rb
+++ lib/vent/publishers/test_publisher.rb
@@ -4,12 +4,11 @@ module Vent
   class TestPublisher < Vent::Publisher
 
     def self.queue
-      @queue ||= {}
+      @queue ||= []
     end
 
     def self.publish(routing_key, message)
-      queue[routing_key] ||= []
-      queue[routing_key] << { :routing_key => routing_key, :message => message }
+      queue << { :routing_key => routing_key, :message => message }
     end
   end
 end
